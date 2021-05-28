9a1412fc6836b2704cfd0ab1f0695d3130cf50f0
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-05-28 14:39:32 +0200

    fine più bonus per provare su button

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+function myFunction() {
+    var utente = Math.floor(Math.random() * 10);
+    console.log('utente ' + utente);
+    
+    var pc = Math.floor(Math.random() * 10);
+    console.log('pc ' + pc);
+    
+    var risultato = "Pareggio"
+    if ( utente < pc) {
+        risultato = "Hai Perso"
+    } else if ( utente > pc) {
+        risultato = "Hai Vinto"
+    }
+    
+    console.log("risultato: " + risultato);
+    document.getElementById("dadi").innerHTML = risultato
+
+}
